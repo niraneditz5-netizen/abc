6e05ae37ae57ed4f792764cd24910b40a13614ce
niraneditz5-netizen <niraneditz5@gmail.com>
2025-11-20 18:54:36 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 
-SESSION_ID: process.env.SESSION_ID === undefined ? '𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=bf5361e4af1e264a' : process.env.SESSION_ID,
+SESSION_ID: process.env.SESSION_ID === undefined '𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=c10256905576b8ce' : process.env.SESSION_ID,
 PORT: process.env.PORT === undefined ? "8000" : process.env.PORT,
 SESSION_NAME: process.env.PORT === undefined ? "asitha" : process.env.SESSION_NAME,
 };
